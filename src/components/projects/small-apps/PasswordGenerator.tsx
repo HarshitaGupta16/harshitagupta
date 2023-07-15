8fb6ea31bffa386cf944637bc1e9442cb2b0d4d6
HarshitaGupta16 <sign.of.happiness@gmail.com>
2023-07-15 15:27:37 +0530

    retried Password Generator App and handled edge cases & strength feature

--- src/components/projects/small-apps/PasswordGenerator.tsx
+++ src/components/projects/small-apps/PasswordGenerator.tsx
@@ -1,77 +1,128 @@
-import { useState } from "react";
 import styles from "./PasswordGenerator.module.css";
+import { useState } from "react";
 
 const PasswordGenerator = () => {
-  const [password, setPassword] = useState("");
   const [passwordLength, setPasswordLength] = useState(4);
-  const [copied, setCopied] = useState(false);
   const [upperCase, setUpperCase] = useState(false);
   const [lowerCase, setLowerCase] = useState(false);
-  const [number, setNumber] = useState(false);
+  const [numbers, setNumbers] = useState(false);
   const [symbols, setSymbols] = useState(false);
+  const [password, setPassword] = useState("");
+  const [copied, setCopied] = useState(false);
+  const [strength, setStrength] = useState("");
+  const [error, setError] = useState("");
 
-  const copyContent = async () => {
+  // console.log(typeof crypto.getRandomValues(new Uint32Array(4)));
+  //   console.log(Array.from(crypto.getRandomValues(new Uint32Array(4))));
+  //   let req = "ABCDEFGHJKLMNPQRSTUVWXYZ";
+  //   console.log("ABCDEFGHJKLMNPQRSTUVWXYZ"[258606595 % 26]);
+
+  const generateRandomValue = (requiredCondition: string, len: number) => {
+    console.log(Array.from(crypto.getRandomValues(new Uint32Array(0))));
+    if (len > 0) {
+      return Array.from(crypto.getRandomValues(new Uint32Array(len)))
+        .map((x) => requiredCondition[x % len])
+        .join("");
+    } else {
+      return "";
+    }
+  };
+
+  const generatePassword = () => {
+    let condition = "";
+    let atLeastOne = "";
+    let pwdLength = passwordLength;
+    if (!upperCase && !lowerCase && !numbers && !symbols) {
+      setError("Please select atleast one checkbox");
+      return;
+    }
+    if (upperCase) {
+      condition += "ABCDEFGHJKLMNPQRSTUVWXYZ";
+      atLeastOne += generateRandomValue("ABCDEFGHJKLMNPQRSTUVWXYZ", 1);
+      pwdLength = pwdLength > 0 ? pwdLength - 1 : 0;
+    }
+    if (lowerCase) {
+      condition += "abcdefghjkmnpqrstuvwxyz";
+      atLeastOne += generateRandomValue("abcdefghjkmnpqrstuvwxyz", 1);
+      pwdLength = pwdLength > 0 ? pwdLength - 1 : 0;
+    }
+    if (numbers) {
+      condition += "1234567890";
+      atLeastOne += generateRandomValue("1234567890", 1);
+      pwdLength = pwdLength > 0 ? pwdLength - 1 : 0;
+    }
+    if (symbols) {
+      condition += "%^&@*#(){}!$~";
+      atLeastOne += generateRandomValue("%^&@*#(){}!$~", 1);
+      pwdLength = pwdLength > 0 ? pwdLength - 1 : 0;
+    }
+    setError("");
+    const randomValue = generateRandomValue(condition, pwdLength);
+    console.log(randomValue);
+    const combineString = atLeastOne + randomValue;
+    setPassword([...combineString].sort(() => 0.5 - Math.random()).join(""));
+    calculateStrength();
+  };
+
+  const handleCopyPassword = async () => {
+    setCopied(true);
     try {
       await navigator.clipboard.writeText(password);
-      setCopied(true);
       setTimeout(() => {
         setCopied(false);
       }, 3000);
     } catch (error) {
       console.log("Failed to copy: ", error);
+      setCopied(false);
     }
   };
 
-  const getRandomValue = (requiredCondition: string, len: number) => {
-    console.log(requiredCondition);
-    console.log(len);
-    return Array.from(crypto.getRandomValues(new Uint32Array(len)))
-      .map((x) => requiredCondition[x % requiredCondition.length])
-      .join("");
-  };
-
-  const generatePassword = () => {
-    let condition = "";
-    if (upperCase) {
-      console.log("upper");
-      condition += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const calculateStrength = () => {
+    if (passwordLength < 1) {
+      setStrength("");
+    } else if (passwordLength < 4) {
+      setStrength("Very Week");
+    } else if (passwordLength < 8) {
+      setStrength("Poor");
+    } else if (passwordLength < 12) {
+      setStrength("Medium");
+    } else if (passwordLength < 16) {
+      setStrength("Strong");
+    } else {
+      setStrength("Very Strong");
     }
-
-    if (lowerCase) {
-      condition += "abcdefghijklmnopqrstuvwxyz";
-    }
-
-    if (number) {
-      condition += "0123456789";
-    }
-
-    if (symbols) {
-      condition += "~!@-#$";
-    }
-
-    const randomValue = getRandomValue(condition, passwordLength);
-    setPassword(randomValue);
   };
 
   return (
     <div className={styles.main}>
-      <div className={styles.header}>
-        <h1>Password Generator</h1>
-      </div>
+      <h1>Password Generator</h1>
       <div className={styles.container}>
-        <div className={styles["sub-header"]}>
-          <span>{password}</span>
-          <button onClick={copyContent}>{copied ? "copied 🗸" : "Copy"}</button>
-        </div>
+        {password !== "" && (
+          <div className={styles.header}>
+            {/* PQ3#@!de */}
+            <span>{password}</span>
+            <button onClick={handleCopyPassword}>
+              {copied ? "Copied 🗸" : "Copy"}
+            </button>
+          </div>
+        )}
         <div className={styles.charLength}>
-          <span className={styles.charLengthLabel}>
-            <label>Character Length</label>
-            <label>{passwordLength}</label>
-          </span>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              marginBottom: 24,
+            }}
+          >
+            <div>Character Length</div>
+            <div>{passwordLength}</div>
+          </div>
           <input
             type="range"
+            style={{ width: "100%" }}
             min={4}
             max={20}
+            value={passwordLength}
             onChange={(e) => setPasswordLength(Number(e.target.value))}
           />
         </div>
@@ -79,48 +130,44 @@ const PasswordGenerator = () => {
           <div>
             <input
               type="checkbox"
-              onChange={() => {
-                setUpperCase((upperCase) => !upperCase);
-              }}
               checked={upperCase}
+              onChange={() => setUpperCase((upperCase) => !upperCase)}
             />
-            <label>Uppercase</label>
+            <label>Include Uppercase Letters</label>
           </div>
           <div>
             <input
               type="checkbox"
-              onChange={() => {
-                setLowerCase((lowerCase) => !lowerCase);
-              }}
               checked={lowerCase}
+              onChange={() => setLowerCase((lowerCase) => !lowerCase)}
             />
-            <label>Lowercase</label>
+            <label>Include Lowercase Letters</label>
           </div>
           <div>
             <input
               type="checkbox"
-              onChange={() => {
-                setNumber((number) => !number);
-              }}
-              checked={number}
+              checked={numbers}
+              onChange={() => setNumbers((numbers) => !numbers)}
             />
-            <label>Number</label>
+            <label>Include Numbers</label>
           </div>
           <div>
             <input
               type="checkbox"
-              onChange={() => {
-                setSymbols((symbols) => !symbols);
-              }}
+              checked={symbols}
+              onChange={() => setSymbols((symbols) => !symbols)}
             />
-            <label>Symbols</label>
+            <label>Include Symbols</label>
           </div>
+          {error && <div style={{ color: "red", marginTop: 10 }}>{error}</div>}
         </div>
-        <div className={styles.strength}>
-          <span>Strength: </span>
-          <span>Medium</span>
-        </div>
-        <button className={styles.genPasswordBtn} onClick={generatePassword}>
+        {strength && (
+          <div className={styles.strength}>
+            <span>Strength: </span>
+            <span>{strength}</span>
+          </div>
+        )}
+        <button className={styles.generatePassword} onClick={generatePassword}>
           Generate Password
         </button>
       </div>
